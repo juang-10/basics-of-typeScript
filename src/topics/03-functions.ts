1045f6587a511101f12718f62150430c9214b39b
juan gonzalez <juandgonzalezs29@gmail.com>
2025-03-07 07:10:17 -0500

    feat: add character interface and healing function example

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -10,7 +10,32 @@ function multiply(firstNumber: number, secondNumber?: number, base: number = 2) 
   return firstNumber * base;
 }
 
-const result: number = addNumbers(1, 2);
-const resultArrow: string = addNumbersArrow(1, 2);
-const multiplyResult: number = multiply(5);
-console.log({ result, resultArrow });
+// const result: number = addNumbers(1, 2);
+// const resultArrow: string = addNumbersArrow(1, 2);
+// const multiplyResult: number = multiply(5);
+// console.log({ result, resultArrow });
+
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+
+
+const healCharacter = ( character: Character, amount: number) => {
+
+  character.hp += amount;
+
+}
+
+const strider: Character = {
+  name: 'Strider',
+  hp: 100,
+  showHp() {
+    console.log(`HP: ${this.hp}`);
+  }
+}
+
+healCharacter(strider, 20);
+healCharacter(strider, 20);
+strider.showHp();
