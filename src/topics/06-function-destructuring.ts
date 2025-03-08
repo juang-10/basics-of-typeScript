4d1c436fba42defc10c3b85ed8e272b6158df39a
juan gonzalez <juandgonzalezs29@gmail.com>
2025-03-08 17:17:03 -0500

    feat: add tax calculation function example with product interface

---
+++ src/topics/06-function-destructuring.ts
@@ -0,0 +1,43 @@
+interface Product {
+  description: string;
+  price: number;
+}
+
+const phone: Product = {
+  description: "Samsung Galaxy S10",
+  price: 150,
+};
+
+const tablet: Product = {
+  description: "iPad Pro",
+  price: 250,
+};
+
+interface TaxtCalculationOptions {
+  tax: number;
+  products: Product[];
+}
+
+// function taxCalculation({ products, tax }: TaxtCalculationOptions): [number, number] {
+function taxCalculation(options: TaxtCalculationOptions): [number, number] {
+  const { products, tax } = options;
+
+  let total = 0;
+
+  products.forEach(({ price }: Pick<Product, "price">) => {
+    total += price;
+  });
+
+  return [total, total * tax];
+}
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+const [total, taxTotal] = taxCalculation({
+  products: shoppingCart,
+  tax,
+});
+
+console.log("🚀 ~ Total", total);
+console.log("🚀 ~ Tax", taxTotal);
